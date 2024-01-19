9fd93a4d86fde24b2cd1b0947a2f8eff2f874618
Lazaro Resendiz Donjuan <rkirisho@gmail.com>
2024-01-19 16:10:43 -0600

    Add sort images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
     <link href="{{ asset('assets/css/table.css') }}" rel="stylesheet"/>
+    {{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"> --}}
     <!-- Scripts -->
     <script src="https://cdn.tailwindcss.com"></script>
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
